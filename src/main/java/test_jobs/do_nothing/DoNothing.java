26e91f1fece920f37c121b1681ce9faf38c8dd9b
Ned Pummeroy <nedpummeroy@gmail.com>
2015-05-11 20:21:06 +1000

    Fixed bug with the redirection of spawned process' stderr

--- src/main/java/test_jobs/do_nothing/DoNothing.java
+++ src/main/java/test_jobs/do_nothing/DoNothing.java
@@ -11,6 +11,9 @@ import java.nio.file.Paths;
  * @author nedp
  */
 class DoNothing {
+    private static final String STDOUT = "stdout: Doing nothing...";
+    private static final String STDERR = "stderr: Done";
+
     private static final int EXIT_SUCCESS = 0;
     private static final int EXIT_FAILURE = 1;
 
@@ -18,19 +21,22 @@ class DoNothing {
         final Path in = Paths.get(args[0]);
         final Path out = Paths.get(args[1]);
 
-        System.out.println("Doing nothing...");
+        System.out.println(STDOUT);
 
         // Check that we were called correctly.
         try {
             if (!in.toFile().canRead() || !out.toFile().createNewFile()) {
+                System.err.println(STDERR);
                 System.exit(EXIT_FAILURE);
             }
         } catch (IOException e) {
+            System.err.println(STDERR);
             System.exit(EXIT_FAILURE);
         }
 
         // Do nothing
-        
+
+        System.err.println(STDERR);
         System.exit(EXIT_SUCCESS);
     }
 }
