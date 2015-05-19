1610ce818b5421589e7578e63c7aae883279f973
Ned Pummeroy <nedpummeroy@gmail.com>
2015-05-19 17:03:11 +1000

    Began implementing WorkerPool.
    Changed some names.
    Added the constructor and #allocateAndExecute(Job) to WorkerPool.
    Round robin allocation is used.

--- src/main/java/com/github/nedp/comp90015/proj2/job/worker/master/WorkerPool.java
+++ src/main/java/com/github/nedp/comp90015/proj2/job/worker/master/WorkerPool.java
@@ -1,13 +1,30 @@
 package com.github.nedp.comp90015.proj2.job.worker.master;
 
 import com.github.nedp.comp90015.proj2.job.Job;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 /**
  * TODO Created by nedp on 18/05/15.
  */
 public class WorkerPool {
+    @NotNull
+    private final List<Worker> workers; // Must be a List for round robin.
+
+    private int iWorker = 0;
+
+    WorkerPool(List<Worker> initialWorkers) {
+        this.workers = initialWorkers;
+    }
+
     // TODO
-    Result allocateAndSend(Job job) throws WorkerUnavailableException {
-        return null;
+    Result allocateAndExecute(Job job) throws WorkerUnavailableException {
+        this.workers.get(iWorker).execute(job);
+        this.iWorker += 1;
+        if (this.iWorker >= this.workers.size()) {
+            this.iWorker = 0;
+        }
+        return Result.FINISHED;
     }
 }
