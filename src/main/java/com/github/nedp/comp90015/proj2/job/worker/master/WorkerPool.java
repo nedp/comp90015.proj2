9b4913dee2ad43509857af7eebe953a95e997506
Ned Pummeroy <nedpummeroy@gmail.com>
2015-05-19 20:42:53 +1000

    Added documentation and error handling to WorkerPool
    #allocateAndExecute throws an exception if the pool is empty

--- src/main/java/com/github/nedp/comp90015/proj2/job/worker/master/WorkerPool.java
+++ src/main/java/com/github/nedp/comp90015/proj2/job/worker/master/WorkerPool.java
@@ -6,7 +6,12 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 
 /**
- * TODO Created by nedp on 18/05/15.
+ * Manages a set of {@link Worker}s, and allocates {@link Job}s to them.
+ * <p/>
+ * In the current implementation, round robin allocation is used.
+ * TODO swap to a more advanced allocation algorithm.
+ *
+ * @author nedp
  */
 public class WorkerPool {
     @NotNull
@@ -14,17 +19,41 @@ public class WorkerPool {
 
     private int iWorker = 0;
 
-    WorkerPool(List<Worker> initialWorkers) {
+    /**
+     * Creates a WorkerPool storing {@link Worker}s in the supplied List.
+     *
+     * @param initialWorkers  the list to store workers in, not null,
+     *                        may be populated.
+     *                        The provided list, not a copy, is injected.
+     */
+    WorkerPool(@NotNull List<Worker> initialWorkers) {
         this.workers = initialWorkers;
     }
 
-    // TODO
+    /**
+     * Allocates the specified {@link Job} to a {@link Worker} and
+     * delegates execution of the Job to the Worker.
+     *
+     * @param job  the job to be executed, not null.
+     * @return the {@link Result} of the job.
+     * @throws WorkerUnavailableException if there are no workers to
+     * allocate the Job to.
+     */
     Result allocateAndExecute(Job job) throws WorkerUnavailableException {
-        this.workers.get(iWorker).execute(job);
-        this.iWorker += 1;
+        final int size = workers.size();
+        // Loop through the workers.
+        // Throw an exception if there are no workers.
         if (this.iWorker >= this.workers.size()) {
+            if (size == 0) {
+                throw new WorkerUnavailableException(
+                    "tried to allocate a job in an empty WorkerPool" );
+            }
             this.iWorker = 0;
         }
-        return Result.FINISHED;
+        // Allocate to the current worker (round robin).
+        final Result result = this.workers.get(iWorker).execute(job);
+        this.iWorker += 1;
+
+        return result;
     }
 }
