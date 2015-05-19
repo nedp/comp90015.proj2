8d22f0a6f0349311a8935f5415dd5748ecd5cdde
Ned Pummeroy <nedpummeroy@gmail.com>
2015-05-19 20:42:53 +1000

    Added documentation and error handling to WorkerPool
    #allocateAndExecute throws an exception if the pool is empty

--- src/test/groovy/com/github/nedp/comp90015/proj2/job/worker/master/WorkerPoolTest.groovy
+++ src/test/groovy/com/github/nedp/comp90015/proj2/job/worker/master/WorkerPoolTest.groovy
@@ -15,14 +15,19 @@ class WorkerPoolTest extends Specification {
         wp = new WorkerPool(workers)
     }
 
-    def "Sends Jobs to a RemoteWorker"() {
-        given:
+    def "Delegates job execution to a Worker"() {
+        given: "there is a mock job and worker producing a given result"
         def job = Mock Job
         def worker = Mock Worker
+        1 * worker.execute(job) >> result
+        and: "the mock worker list 'contains' the mock worker"
         1 * workers.get(_) >> worker
+        workers.size() >> 1
 
-        when: wp.allocateAndExecute(job)
-        then: 1 * worker.execute(job)
+        expect: result == wp.allocateAndExecute(job)
+
+        where:
+        result << [Result.FINISHED, Result.FAILED, Result.DISCONNECTED]
     }
 
     def "Uses round robin allocation"() {
@@ -30,14 +35,28 @@ class WorkerPoolTest extends Specification {
         given:
         def workerList = (1..5).collect { Mock(Worker) }
         def jobs = (1..13).collect { Mock(Job) }
+        workers.size() >> 5
 
         when: jobs.each { job -> wp.allocateAndExecute(job) }
         then:
-        13 * workers.size() >> 5
         workerList.eachWithIndex { worker, i ->
             def n = 1 + (12-i).intdiv(5)
             n * workers.get(i % 5) >> workerList[i % 5]
         }
         jobs.eachWithIndex { job, i -> 1 * workerList[i % 5].execute(job) }
+    }
+
+    def "Throws WorkerUnavailableExceptions if the pool is empty"() {
+        0 * _
+        workers.size() >> 0
+        when: wp.allocateAndExecute(Mock(Job))
+        then: thrown(WorkerUnavailableException)
+    }
+
+    def "Throws WorkerUnavailableExceptions if the pool is empty"() {
+        0 * _
+        workers.size() >> 0
+        when: wp.allocateAndExecute(Mock(Job))
+        then: thrown(WorkerUnavailableException)
     }
 }
