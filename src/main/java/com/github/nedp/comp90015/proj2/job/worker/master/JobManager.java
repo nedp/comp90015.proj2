dc7ffdddcb8aa41fe9e7f749a82db796fe5d42a3
Ned Pummeroy <nedpummeroy@gmail.com>
2015-05-22 20:16:33 +1000

    Refactored JobManager to use **client side** job ids
    * Added a new record class JobResult to link Jobs to their Result
    * Changed the Map<Job, Result> in JobManager to an ArrayList<JobResult>
    * Rewrote tests in a better (more natural) style, reflected the interface changes

--- src/main/java/com/github/nedp/comp90015/proj2/job/worker/master/JobManager.java
+++ src/main/java/com/github/nedp/comp90015/proj2/job/worker/master/JobManager.java
@@ -3,6 +3,8 @@ package com.github.nedp.comp90015.proj2.job.worker.master;
 import com.github.nedp.comp90015.proj2.job.Job;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -19,7 +21,7 @@ import java.util.Optional;
  */
 public class JobManager {
     @NotNull
-    private final Map<Job, Optional<Result>> jobResults;
+    private final List<JobResult> jobResults;
     @NotNull
     private final WorkerPool pool;
 
@@ -29,11 +31,24 @@ public class JobManager {
      * These instances may be pre-initialised before being injected.
      *
      * @param pool should be ready to allocate Jobs, not null
-     * @param jobResults should be ready to store Job/Result pairs, not null
      */
-    JobManager(@NotNull WorkerPool pool, @NotNull Map<Job, Optional<Result>> jobResults) {
+    JobManager(@NotNull WorkerPool pool) {
         this.pool = pool;
-        this.jobResults = jobResults;
+        this.jobResults = new ArrayList<>();
+    }
+
+    /**
+     * Submits a {@link Job} for subsequent execution.
+     *
+     * @param job the Job to be executed, not null
+     * @return the unique (within this JobManager)
+     * positive integer id of the submitted Job.
+     */
+    public synchronized int submit(@NotNull Job job) {
+        final int id = this.jobResults.size();
+        final boolean ok = this.jobResults.add(new JobResult(job, Optional.empty()));
+        assert(ok);
+        return id;
     }
 
     /**
@@ -44,36 +59,47 @@ public class JobManager {
      * The method will return when the job terminates.
      * It has unspecified behaviour when called twice for the same Job.
      *
-     * @param job the Job to be executed, not null
+     * @param id  the unique integer id of the Job to be executed.
      * @return the Result of the Job, after it has executed
      * @throws WorkerUnavailableException when this' WorkerPool has no
      *                                    available Workers.
      */
     @NotNull
-    public Result execute(@NotNull Job job) throws WorkerUnavailableException {
-        this.jobResults.put(job, Optional.empty());
+    public Result execute(int id) throws WorkerUnavailableException {
+        final Job job = this.jobResults.get(id).job;
         final Result result = this.pool.allocateAndExecute(job);
-        assert(Optional.empty().equals(
-            this.jobResults.replace(job, Optional.of(result))));
+
+        assert(Optional.empty().equals(this.jobResults.get(id).result));
+        this.jobResults.set(id, new JobResult(job, Optional.of(result)));
         return result;
     }
 
     /**
      * Returns the {@link Result} of the specified {@link Job}.
      *
-     * @param job the Job of interest
+     * @param id  the integer id of the Job of interest.
      * @return Optional.of(Result) if the Job has terminated,
      *         otherwise Optional.empty
      * @throws IndexOutOfBoundsException if the job is not tracked.
      */
     @NotNull
-    public Optional<Result> resultOf(@NotNull Job job) {
-        final Optional<Result> result = this.jobResults.get(job);
-        if (result == null) {
+    public Optional<Result> resultOf(int id) {
+        final JobResult jobResult = this.jobResults.get(id);
+        if (jobResult == null) {
             throw new IndexOutOfBoundsException(String.format(
-                "requested job is not tracked: %s", job));
+                "no job with the requested id (%d) is tracked", id));
         } else {
-            return result;
+            return jobResult.result;
+        }
+    }
+
+    private class JobResult {
+        @NotNull private final Job job;
+        @NotNull private final Optional<Result> result;
+
+        private JobResult(@NotNull Job job, @NotNull Optional<Result> result) {
+            this.job = job;
+            this.result = result;
         }
     }
 }
