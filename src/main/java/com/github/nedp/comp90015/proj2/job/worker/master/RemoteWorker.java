7a42f6772332b524eede3e4725480982bd4db215
Charles Talbot <ctalbot@student.unimelb.edu.au>
2015-05-30 22:29:05 +1000

    Master now should receive an update on the free memory available for each Worker

--- src/main/java/com/github/nedp/comp90015/proj2/job/worker/master/RemoteWorker.java
+++ src/main/java/com/github/nedp/comp90015/proj2/job/worker/master/RemoteWorker.java
@@ -1,8 +1,8 @@
 package com.github.nedp.comp90015.proj2.job.worker.master;
 
-import java.io.DataInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.SocketAddress;
+import java.io.InputStreamReader;
 import java.net.UnknownHostException;
 
 import javax.net.ssl.SSLSocket;
@@ -19,17 +19,27 @@ import org.jetbrains.annotations.NotNull;
 public class RemoteWorker implements Worker {
     private final String hostname;
     private final int port;
+    private final int jobPort;
     private SSLSocket workerSocket;
+    private long freeMemory;
 
     public RemoteWorker(@NotNull String hostname, int port) throws UnknownHostException, IOException {
         this.hostname = hostname;
         this.port = port;
         SSLSocketFactory factory = (SSLSocketFactory)SSLSocketFactory.getDefault();
         workerSocket = (SSLSocket) factory.createSocket(hostname,port);
-        DataInputStream input = new DataInputStream(workerSocket.getInputStream());
+        
+        // Get the port for the job from the Worker
+        BufferedReader input = new BufferedReader(new InputStreamReader(workerSocket.getInputStream()));
+        jobPort = Integer.parseInt(input.readLine().split(":")[1]);
+        
+        // Begins the status update thread
+        Thread memoryThread = new Thread(new MemoryReceiver(this, workerSocket));
+        memoryThread.setDaemon(true);
+        
     }
 
-    @NotNull
+	@NotNull
     @Override
     public Result execute(Job job) {
         return Result.DISCONNECTED; // TODO
@@ -49,6 +59,10 @@ public class RemoteWorker implements Worker {
     @Override
     public long freeMemory() {
         return 0; // TODO
+    }
+    
+    public void setFreeMemory(long freeMemory){
+    	this.freeMemory = freeMemory;
     }
 
     @NotNull
