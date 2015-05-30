fddb96aa41c0bc48ecb8453ef94adbd4ae70603a
James McMahon <whichoneiwonder@gmail.com>
2015-05-30 13:06:10 +1000

    Created a a class for each worker thread that's going to be run (I.E. Worker-main thread, remote Master thread, jobHandler thread) TODO : do the marshalling/unmarshalling on jobs TODO : write job files to disk TODO : marshalling and unmarshalling the return file TODO : erase job files from disk after completion

--- src/main/java/com/github/nedp/comp90015/proj2/job/worker/WorkerMain.java
+++ src/main/java/com/github/nedp/comp90015/proj2/job/worker/WorkerMain.java
@@ -4,19 +4,24 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
-import com.github.nedp.comp90015.proj2.job.Job;
-import com.github.nedp.comp90015.proj2.job.Status;
-import com.github.nedp.comp90015.proj2.job.StatusTracker;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
+
+import com.github.nedp.comp90015.proj2.job.*;
 
 
-public class Worker {
+public class WorkerMain {
 
 	
 	static final int defaultPort = 80;
+	static ArrayList<RemoteMaster> masterList;
 
 	public static void main(String[] args) throws InterruptedException, IOException {
-		
+		masterList = new ArrayList<RemoteMaster>();
+		boolean keepRunning= true;
 		int portNumber = defaultPort;
 		
 		for(int i = 0; i +1 < args.length; i++ ){
@@ -28,14 +33,25 @@ public class Worker {
 		System.out.println("portnum is " + portNumber);
 		
 		// open listening port
-		
-		
+		SSLServerSocketFactory ssf = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();	
+		SSLServerSocket ss = (SSLServerSocket) ssf.createServerSocket(portNumber);
 		// accept connections and create new 'remote master' for each
+		while(keepRunning){
+			try{
+				SSLSocket sock = (SSLSocket) ss.accept();
 		
-		// 
+				RemoteMaster master = new RemoteMaster(sock, ssf);
+				masterList.add(master);
+				Thread t = new Thread(master);
+				t.start();
+			
+			} catch(Exception e){
+				break;
+			}
+			
+		}
 		
 		// accept jobs from each master
-		
 		//dummyjob
 		/*specify the job specifically*/
 
@@ -84,6 +100,10 @@ public class Worker {
 		fr.close();
 		
 
+	}
+	
+	protected static boolean masterDisconnected(RemoteMaster r){
+		return masterList.remove(r);
 	}
 	
 
